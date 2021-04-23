d7854c1aefe730ef45e1352e5737714f266c3404
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-04-23 11:42:45 -0700

    complete step 3.2

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+class Item(models.Model):
+    description = models.TextField(max_length=50)
 
+    def __str__(self):
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse('index')
