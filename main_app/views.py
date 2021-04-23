298538ead2913e61cdd939a170866d156046d164
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-04-23 12:31:07 -0700

    complete step 3.3

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def index(request):
     items = Item.objects.all()
     return render(request, 'index.html', { 'items' : items})
+
+class Add(CreateView):
+    model= Item
+    fields = '__all__'
+
